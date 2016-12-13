23c131cb5f0c1812c27692fb080709c32797d68b
alv1r <alv1r@ya.ru>
2016-12-13 17:55:33 +0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,6 +2,6 @@ apt-get install apache2 php libapache2-mod-php php-fpm
 wget https://github.com/alv1r/SqStat/archive/master.zip
 unzip master.zip 
 mv SqStat-master /var/www/sqstat
-cp /var/www/sqstat/sqstat.conf /etc/apache2/sites-avaliable/sqstat.conf
+cp /var/www/sqstat/sqstat.conf /etc/apache2/sites-avaliable
 a2ensite sqstat.conf 
 service apache2 reload
